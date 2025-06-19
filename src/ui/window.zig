e1db8a2e4ecc1bb21d64332f0e5e2f90d5230382
gaoxiangkang <1148118271@qq.com>
2025-06-19 23:19:27 +0800

    2025-6-19 23:19

--- src/ui/window.zig
+++ src/ui/window.zig
@@ -1,4 +1,47 @@
+const std = @import("std");
+const sdl = @import("../c.zig").sdl;
+
 pub const WindowError = error{
     SdlInit,
     CreateWindow,
 };
+
+var window: ?*sdl.SDL_Window = null;
+var event: sdl.SDL_Event = undefined;
+var running: bool = true;
+
+pub fn create() WindowError!void {
+    std.debug.print("create......\n", .{});
+    if (!sdl.SDL_Init(sdl.SDL_INIT_VIDEO)) {
+        std.debug.print("SDL_Init Error: {s}\n", .{sdl.SDL_GetError()});
+        return WindowError.SdlInit;
+    }
+    window = sdl.SDL_CreateWindow("My Window", 800, 600, sdl.SDL_WINDOW_RESIZABLE);
+    if (window == null) {
+        std.debug.print("SDL_CreateWindow Error: {s}\n", .{sdl.SDL_GetError()});
+        sdl.SDL_Quit();
+        return WindowError.CreateWindow;
+    }
+}
+
+pub fn run() void {
+    std.debug.print("run......\n", .{});
+    while (running) {
+        _ = sdl.SDL_WaitEvent(&event);
+        switch (event.type) {
+            sdl.SDL_EVENT_QUIT => {
+                running = false;
+                std.debug.print("quit......\n", .{});
+                break;
+            },
+            else => {},
+        }
+        // SDL_Delay(16);
+    }
+}
+
+pub fn close() void {
+    std.debug.print("close......\n", .{});
+    sdl.SDL_DestroyWindow(window);
+    sdl.SDL_Quit();
+}
